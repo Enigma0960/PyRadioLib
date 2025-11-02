5ae3b5a31c1d3bce0e7be80d5f224a0aa17e7147
enigma0960 <supryadkin@uniscan.biz>
2025-11-02 16:45:13 +0700

    New cmake logic end

---
+++ cmake/Modules/Findpybind11.cmake
@@ -0,0 +1,16 @@
+if (APPLE)
+    find_path(Pybind11_DIR
+            NAMES CMakeLists.txt
+            HINTS ${PYBIND11_DIR}
+            PATHS ${PYBIND11_DIR}
+    )
+endif ()
+
+
+mark_as_advanced(Pybind11_DIR)
+
+add_subdirectory("${Pybind11_DIR}" "${CMAKE_BINARY_DIR}/_deps/pybind11-subbuild" EXCLUDE_FROM_ALL)
+set(pybind11_FOUND TRUE)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(pybind11 REQUIRED_VARS pybind11_FOUND)
--- cmake/Modules/Findradiolib.cmake
+++ cmake/Modules/Findradiolib.cmake
@@ -1,13 +1,11 @@
-# Radiolib_SOURCE_DIR
-
-
 if (APPLE)
-    find_path(Radiolib_HOME_DIR NAMES library.json
-            HINTS ${RADIOLIB_HOME_DIR}
-            PATHS ${RADIOLIB_HOME_DIR}
+    find_path(Radiolib_DIR
+            NAMES library.json
+            HINTS ${RADIOLIB_DIR}
+            PATHS ${RADIOLIB_DIR}
     )
-
-    find_path(Radiolib_SOURCE_DIR NAMES RadioLib.h
+    find_path(Radiolib_SOURCE_DIR
+            NAMES RadioLib.h
             HINTS ${RADIOLIB_SOURCE_DIR}
             PATHS ${RADIOLIB_SOURCE_DIR}
     )
@@ -17,8 +15,8 @@ mark_as_advanced(Radiolib_SOURCE_DIR)
 
 collect_sources(${Radiolib_SOURCE_DIR} RADIOLIB_HEADERS RADIOLIB_SOURCES)
 
-if (NOT Radiolib_VERSION AND EXISTS "${Radiolib_HOME_DIR}/library.json")
-    file(STRINGS "${Radiolib_HOME_DIR}/library.json" RADIOLIV_VERSION_LINE REGEX "\"version\":")
+if (NOT Radiolib_VERSION AND EXISTS "${Radiolib_DIR}/library.json")
+    file(STRINGS "${Radiolib_DIR}/library.json" RADIOLIV_VERSION_LINE REGEX "\"version\":")
     string(REGEX REPLACE "[ \t]*\"version\":[ \t]*\"" "" Radiolib_VERSION ${RADIOLIV_VERSION_LINE})
     string(REGEX REPLACE "\"," "" Radiolib_VERSION ${Radiolib_VERSION})
 endif ()
@@ -28,8 +26,7 @@ set(COPY_SCRIPT "${CMAKE_BINARY_DIR}/copy_includes.cmake")
 file(WRITE "${COPY_SCRIPT}" "file(MAKE_DIRECTORY \"${DEST_DIR}\")\n")
 foreach (src ${SRC_FILES})
     get_filename_component(fname "${src}" NAME)
-    file(APPEND "${COPY_SCRIPT}"
-            "file(COPY \"${src}\" DESTINATION \"${DEST_DIR}\")\n")
+    file(APPEND "${COPY_SCRIPT}" "file(COPY \"${src}\" DESTINATION \"${DEST_DIR}\")\n")
 endforeach ()
 
 set(Radiolib_INCLUDE_DIR "${CMAKE_BINARY_DIR}/include/radiolib")
@@ -43,6 +40,9 @@ copy_headers(
 
 add_library(radiolib STATIC SHARED ${RADIOLIB_HEADERS} ${RADIOLIB_SOURCES})
 add_dependencies(radiolib radiolib_include)
+target_include_directories(radiolib SYSTEM PRIVATE
+        ${Radiolib_INCLUDE_DIR}
+)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
